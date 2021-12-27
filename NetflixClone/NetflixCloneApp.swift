cb9d57623984bdbaf073641b63a682e3abff0719
Guillaume Moigneu <g@moigneu.com>
2021-12-27 06:20:13 -0600

    Initial Commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Guillaume Moigneu on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
